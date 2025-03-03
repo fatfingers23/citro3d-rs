d191436dd8b3c86f2b47b76720f6d41f9c1ebd69
Bailey Townsend <baileytownsend2323@gmail.com>
2025-03-02 18:38:04 -0600

    Cleaned up warnings

--- citro2d/examples/2d_shapes.rs
+++ citro2d/examples/2d_shapes.rs
@@ -1,14 +1,12 @@
 //! This example demonstrates the most basic usage of `citro2d`: rendering shapes
 //! on the top screen of the 3DS.
-
+//! This is an exact copy of 2d_shapes from the devkitPro examples, but in Rust.
+//! https://github.com/devkitPro/3ds-examples/blob/master/graphics/gpu/2d_shapes/source/main.c
 #![feature(allocator_api)]
 
 use citro2d::render::{Color, Target};
-use citro2d::shapes::{self, Circle, CircleSolid, Ellipse, Rectangle, Shape, Triangle};
-use ctru::{
-    prelude::*,
-    services::gfx::{BottomScreen, TopScreen3D},
-};
+use citro2d::shapes::{Circle, CircleSolid, Ellipse, Rectangle, Triangle};
+use ctru::{prelude::*, services::gfx::TopScreen3D};
 
 const SCREEN_WIDTH: u16 = 400;
 const SCREEN_HEIGHT: u16 = 240;
@@ -20,42 +18,42 @@ fn main() {
 
     let mut citro2d_instance = citro2d::Instance::new().expect("Couldn't obtain citro2d instance");
     let top_screen = TopScreen3D::from(&gfx.top_screen);
-    let (mut top_left, _) = top_screen.split_mut();
+    let (top_left, _) = top_screen.split_mut();
     let mut top_target = Target::new(top_left).expect("failed to create render target");
 
     let bottom_screen = Console::new(gfx.bottom_screen.borrow_mut());
-    let clrWhite = Color::new_with_alpha(0xFF, 0xFF, 0xFF, 0xFF);
-    let clrGreen = Color::new_with_alpha(0x00, 0xFF, 0x00, 0xFF);
-    let clrRed = Color::new_with_alpha(0xFF, 0x00, 0x00, 0xFF);
-    let clrBlue = Color::new_with_alpha(0x00, 0x00, 0xFF, 0xFF);
-    let clrCircle1 = Color::new_with_alpha(0xFF, 0x00, 0xFF, 0xFF);
-    let clrCircle2 = Color::new_with_alpha(0xFF, 0xFF, 0x00, 0xFF);
-    let clrCircle3 = Color::new_with_alpha(0x00, 0xFF, 0xFF, 0xFF);
-    let clrSolidCircle = Color::new_with_alpha(0x68, 0xB0, 0xD8, 0xFF);
-    let clrTri1 = Color::new_with_alpha(0xFF, 0x15, 0x00, 0xFF);
-    let clrTri2 = Color::new_with_alpha(0x27, 0x69, 0xE5, 0xFF);
-    let clrRec1 = Color::new_with_alpha(0x9A, 0x6C, 0xB9, 0xFF);
-    let clrRec2 = Color::new_with_alpha(0xFF, 0xFF, 0x2C, 0xFF);
-    let clrRec3 = Color::new_with_alpha(0xD8, 0xF6, 0x0F, 0xFF);
-    let clrRec4 = Color::new_with_alpha(0x40, 0xEA, 0x87, 0xFF);
-    let clrClear = Color::new_with_alpha(0xFF, 0xD8, 0xB0, 0x68);
+    let clr_white = Color::new_with_alpha(0xFF, 0xFF, 0xFF, 0xFF);
+    let clr_green = Color::new_with_alpha(0x00, 0xFF, 0x00, 0xFF);
+    let clr_red = Color::new_with_alpha(0xFF, 0x00, 0x00, 0xFF);
+    let clr_blue = Color::new_with_alpha(0x00, 0x00, 0xFF, 0xFF);
+    let clr_circle1 = Color::new_with_alpha(0xFF, 0x00, 0xFF, 0xFF);
+    let clr_circle2 = Color::new_with_alpha(0xFF, 0xFF, 0x00, 0xFF);
+    let clr_circle3 = Color::new_with_alpha(0x00, 0xFF, 0xFF, 0xFF);
+    let clr_solid_circle = Color::new_with_alpha(0x68, 0xB0, 0xD8, 0xFF);
+    let clr_tri1 = Color::new_with_alpha(0xFF, 0x15, 0x00, 0xFF);
+    let clr_tri2 = Color::new_with_alpha(0x27, 0x69, 0xE5, 0xFF);
+    let clr_rec1 = Color::new_with_alpha(0x9A, 0x6C, 0xB9, 0xFF);
+    let clr_rec2 = Color::new_with_alpha(0xFF, 0xFF, 0x2C, 0xFF);
+    let clr_rec3 = Color::new_with_alpha(0xD8, 0xF6, 0x0F, 0xFF);
+    let clr_rec4 = Color::new_with_alpha(0x40, 0xEA, 0x87, 0xFF);
+    let clr_clear = Color::new_with_alpha(0xFF, 0xD8, 0xB0, 0x68);
 
     while apt.main_loop() {
         hid.scan_input();
 
-        citro2d_instance.render_target(&mut top_target, |_instance, render_target| unsafe {
-            render_target.clear(clrClear);
+        citro2d_instance.render_target(&mut top_target, |_instance, render_target| {
+            render_target.clear(clr_clear);
 
             render_target.render_2d_shape(&Triangle {
                 x0: 25.0,
                 y0: 190.0,
-                color0: clrWhite,
+                color0: clr_white,
                 x1: 0.0,
                 y1: SCREEN_HEIGHT as f32,
-                color1: clrTri1,
+                color1: clr_tri1,
                 x2: 50.0,
                 y2: SCREEN_HEIGHT as f32,
-                color2: clrTri2,
+                color2: clr_tri2,
                 depth: 0.0,
             });
 
@@ -65,10 +63,10 @@ fn main() {
                 z: 0.0,
                 width: 50.0,
                 height: 50.0,
-                color_top_left: clrRec1,
-                color_top_right: clrRec2,
-                color_bottom_left: clrRec3,
-                color_bottom_right: clrRec4,
+                color_top_left: clr_rec1,
+                color_top_right: clr_rec2,
+                color_bottom_left: clr_rec3,
+                color_bottom_right: clr_rec4,
             });
 
             // Circles require a state change (an expensive operation) within citro2d's internals, so draw them last.
@@ -82,10 +80,10 @@ fn main() {
                 z: 0.0,
                 width: SCREEN_WIDTH as f32,
                 height: SCREEN_HEIGHT as f32,
-                top_left_color: clrCircle1,
-                top_right_color: clrCircle2,
-                bottom_left_color: clrCircle3,
-                bottom_right_color: clrWhite,
+                top_left_color: clr_circle1,
+                top_right_color: clr_circle2,
+                bottom_left_color: clr_circle3,
+                bottom_right_color: clr_white,
             });
 
             render_target.render_2d_shape(&Circle {
@@ -93,10 +91,10 @@ fn main() {
                 y: (SCREEN_HEIGHT / 2) as f32,
                 z: 0.0,
                 radius: 50.0,
-                top_left_color: clrCircle3,
-                top_right_color: clrWhite,
-                bottom_left_color: clrCircle1,
-                bottom_right_color: clrCircle2,
+                top_left_color: clr_circle3,
+                top_right_color: clr_white,
+                bottom_left_color: clr_circle1,
+                bottom_right_color: clr_circle2,
             });
 
             render_target.render_2d_shape(&Circle {
@@ -104,10 +102,10 @@ fn main() {
                 y: 25.0,
                 z: 0.0,
                 radius: 25.0,
-                top_left_color: clrRed,
-                top_right_color: clrBlue,
-                bottom_left_color: clrGreen,
-                bottom_right_color: clrWhite,
+                top_left_color: clr_red,
+                top_right_color: clr_blue,
+                bottom_left_color: clr_green,
+                bottom_right_color: clr_white,
             });
 
             render_target.render_2d_shape(&CircleSolid {
@@ -115,7 +113,7 @@ fn main() {
                 y: (SCREEN_HEIGHT - 25) as f32,
                 z: 0.0,
                 radius: 25.0,
-                color: clrSolidCircle,
+                color: clr_solid_circle,
             });
         });
 
