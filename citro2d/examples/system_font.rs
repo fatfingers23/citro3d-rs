028008de080da7407adefc440536f101c623f98e
Bailey Townsend <baileytownsend2323@gmail.com>
2025-03-25 19:02:16 -0500

    more wip for font

--- citro2d/examples/system_font.rs
+++ citro2d/examples/system_font.rs
@@ -4,6 +4,7 @@
 use citro2d::Point;
 use citro2d::render::{Color, Target};
 use citro2d::shapes::{Circle, CircleSolid, Ellipse, MultiColor, Rectangle, Triangle};
+use citro2d::text::{Text, system_font};
 use ctru::{prelude::*, services::gfx::TopScreen3D};
 
 const SCREEN_WIDTH: u16 = 400;
@@ -24,25 +25,33 @@ fn main() {
 
     let white = Color::new(255, 255, 255);
 
-    let c2d_text_buffer = unsafe { citro2d_sys::C2D_TextBufNew(4096) };
-    let mut c2d_text = unsafe {
-        citro2d_sys::C2D_Text {
-            buf: c2d_text_buffer,
-            begin: 0,
-            end: 0,
-            width: 0.0,
-            lines: 0,
-            words: 0,
-            font: citro2d_sys::C2D_FontLoadSystem(0),
-        }
-    };
+    // if let Err(e) = system_font() {
+    //     panic!("Failed to load system font: {:?}", e);
+    // }
+    // let c2d_text_buffer = unsafe { citro2d_sys::C2D_TextBufNew(4096) };
+    // let mut c2d_text = unsafe {
+    //     citro2d_sys::C2D_Text {
+    //         buf: c2d_text_buffer,
+    //         begin: 0,
+    //         end: 0,
+    //         width: 0.0,
+    //         lines: 0,
+    //         words: 0,
+    //         font: citro2d_sys::C2D_FontLoadSystem(0),
+    //     }
+    // };
 
-    unsafe {
-        citro2d_sys::C2D_TextParse(&mut c2d_text, c2d_text_buffer, "DVD".as_ptr() as *const u8)
-    };
-    unsafe {
-        citro2d_sys::C2D_TextOptimize(&c2d_text as *const _);
-    }
+    // unsafe {
+    //     citro2d_sys::C2D_TextParse(&mut c2d_text, c2d_text_buffer, "DVD".as_ptr() as *const u8)
+    // };
+    // unsafe {
+    //     citro2d_sys::C2D_TextOptimize(&c2d_text as *const _);
+    // }
+
+    // let system_font = system_font().expect("Failed to load system font");
+    let mut dvd_text = Text::new().expect("Failed to create text buffer");
+    let _ = dvd_text.parse_and_optimize("DVD");
+
     let mut x = 8.0;
     let mut y = 8.0;
     let mut dx = 1.0;
@@ -55,19 +64,22 @@ fn main() {
         x += dx;
         y += dy;
 
-        // Check for collision with screen edges and reverse direction if needed
-        if x <= 0.0 || x + c2d_text.width > SCREEN_WIDTH as f32 {
-            dx = -dx;
-        }
-        if y <= 0.0 || y + 16.0 > SCREEN_HEIGHT as f32 {
-            dy = -dy;
-        }
+        // // Check for collision with screen edges and reverse direction if needed
+        // if x <= 0.0 || x + dvd_text.c2d_text.width > SCREEN_WIDTH as f32 {
+        //     dx = -dx;
+        // }
+        // if y <= 0.0 || y + 16.0 > SCREEN_HEIGHT as f32 {
+        //     dy = -dy;
+        // }
 
         citro2d_instance.render_target(&mut top_target, |_instance, render_target| {
             render_target.clear(white);
+            // dvd_text.clear_buffer();
+            // dvd_text.draw(Point::new(x, y, 0.5), 1.0, 1.0);
+
             unsafe {
-                citro2d_sys::C2D_TextBufClear(c2d_text_buffer);
-                citro2d_sys::C2D_DrawText(&c2d_text as *const _, 0, x, y, 0.5, 1.0, 1.0);
+                // citro2d_sys::C2D_TextBufClear(dvd_text.buffer);
+                citro2d_sys::C2D_DrawText(&dvd_text.c2d_text as *const _, 0, x, y, 0.5, 1.0, 1.0);
             }
         });
 
--- citro2d/src/text.rs
+++ citro2d/src/text.rs
@@ -1,3 +1,101 @@
+use std::fmt::Error;
+
 // pub fn text_parse() {
 //     unsafe { citro2d_sys::C2D_TextParse(text, buf, str_) }
 // }
+use citro2d_sys::{C2D_Font, C2D_Text, C2D_TextBuf};
+
+use crate::Point;
+
+pub struct Text {
+    pub buffer: C2D_TextBuf,
+    font: C2D_Font,
+    pub c2d_text: C2D_Text,
+}
+
+#[derive(Debug)]
+pub enum TextError {
+    FailedToInitializeBuffer,
+    FailedToInitializeFont,
+    TextParseError,
+}
+
+pub fn system_font() -> Result<C2D_Font, TextError> {
+    let system_font = unsafe { citro2d_sys::C2D_FontLoadSystem(1) };
+    if system_font.is_null() {
+        return Err(TextError::FailedToInitializeFont);
+    }
+    Ok(system_font)
+}
+
+impl Text {
+    pub fn new() -> Result<Self, TextError> {
+        let system_font = unsafe { citro2d_sys::C2D_FontLoadSystem(1) };
+
+        Self::new_with_size(4096, system_font)
+    }
+
+    pub fn new_with_size(size: usize, font: C2D_Font) -> Result<Self, TextError> {
+        let buffer = unsafe { citro2d_sys::C2D_TextBufNew(size) };
+        if buffer.is_null() {
+            return Err(TextError::FailedToInitializeBuffer);
+        }
+
+        let c2d_text = C2D_Text {
+            buf: buffer,
+            //Just setting defaults I think citro2d takes over and sets these
+            begin: 0,
+            end: 0,
+            width: 0.0,
+            lines: 0,
+            words: 0,
+            font,
+        };
+
+        Ok(Self {
+            buffer,
+            font,
+            c2d_text,
+        })
+    }
+
+    pub fn parse_and_optimize(&mut self, text: &str) -> Result<(), TextError> {
+        let parse = unsafe {
+            citro2d_sys::C2D_TextParse(&mut self.c2d_text, self.buffer, text.as_ptr() as *const u8)
+        };
+        // if parse.is_null() {
+        //     return Err(TextError::TextParseError);
+        // }
+        unsafe {
+            citro2d_sys::C2D_TextOptimize(&self.c2d_text);
+        }
+
+        Ok(())
+    }
+
+    pub fn clear_buffer(&mut self) {
+        unsafe {
+            citro2d_sys::C2D_TextBufClear(self.buffer);
+        }
+    }
+
+    pub fn draw(&self, point: Point, scale_x: f32, scale_y: f32) {
+        unsafe {
+            citro2d_sys::C2D_DrawText(
+                &self.c2d_text,
+                0,
+                point.x,
+                point.y,
+                point.x,
+                scale_x,
+                scale_y,
+            );
+        }
+    }
+
+    // pub fn optimize(&mut self) {
+    //     unsafe {
+    //         citro2d_sys::C2D_TextOptimize(&self.buffer as *const _);
+    //     }
+    // }
+}
